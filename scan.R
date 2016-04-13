883e54c38861305915a707b5593564ded341cb62
tnybny <tnybny@gmail.com>
2016-04-13 14:33:37 -0400

    ten random days, outer loop for scan

--- scan.R
+++ scan.R
@@ -15,12 +15,9 @@ require(corpcor) # for pseudoinverse
 palette(diverge_hsv(21))
 
 # load the baseline
-baselinepath <- paste("~/Google Drive/NCSU/STAC lab research/",
-                      "Monster Ridges project/Data/CSVs/",
-                      "Scanning_window/Baseline/", sep = "")
+baselinepath <- paste("~/Documents/Scanning window/Baseline/", sep = "")
 meanBase <- readMat(paste(baselinepath, "meanBaseline.mat", sep = ""))$meanBase
 dir <- integer(1) # direction of extreme (warm (1) or cold (-1))
-# covariance matrix singularity
 p <- integer(1) # dimensionality of trimmed covariance matrix
 
 calcMDsqFromMVG <- function(rectGrid, meanBase, origYData, day, data)
@@ -48,7 +45,7 @@ calcMDsqFromMVG <- function(rectGrid, meanBase, origYData, day, data)
                  rectGrid, meanBase, day)
     # if window spans warm and cold extremes (Quadrants II or IV), skip
     if(!(all((rectGrid[, 3] - mu) >= 0) |
-             all((rectGrid[, 3] - mu) <= 0)))
+         all((rectGrid[, 3] - mu) <= 0)))
     {
         p <<- 0
         return(0)
@@ -76,7 +73,7 @@ covBaseRectScore <- function(rectGrid, origYData, day)
     #
     # Returns
     # COV: covariance matrix
-    d <- matrix(, nrow = 175)
+    d <- matrix(0, nrow = 175)
     for(g in 1:nrow(rectGrid))
     {
         lat = rectGrid[g, 1]
@@ -95,86 +92,92 @@ covBaseRectScore <- function(rectGrid, origYData, day)
 if(!exists("origYData"))
     source("loadData.R")
 
-# for each year
-year = 1979
+# ten random days in the period of record
+years <- c(1985, 1982, 1979, 1999, 1988, 2013, 2011, 1988, 1993, 1994)
+days <- c(360, 77, 269, 203, 219, 89, 167, 169, 143, 92)
 
-# for each day
-day = 3
-data <- origYData[[year - 1978]][day, , ]
+plotpath <- paste("~/Documents/Scanning window/TenRandomDaysPlots/plot%02d.jpg")
+#jpeg(plotpath, width = 1024, height = 1024)
 
-# create object to color
-resToday <- matrix (0, 73, 144)
-
-for (i1 in 1:73){ 
-    print(i1)
-    for (j1 in 1:144){
-        for (i2 in i1:(i1 + 2)){
-            if(i2 > 73){
-                i2 = 73
-            }
-            for (j2 in j1:(j1 + 2)){
-                if(j2 > 144){
-                    j2 = 144
+for(it in 1:length(days)){
+    year <- years[it]
+    day <- days[it]
+    data <- origYData[[year - 1978]][day, , ]
+    
+    # create object to color
+    resToday <- matrix (0, 73, 144)
+    
+    for (i1 in 1:73){ 
+        print(i1)
+        for (j1 in 1:144){
+            for (i2 in i1:(i1 + 2)){
+                if(i2 > 73){
+                    i2 = 73
                 }
-                rectGrid <- data.frame(lat = numeric(), 
-                                       long = numeric(),
-                                       val = double())
-                for(i in i1:i2)
-                {
-                    for(j in j1:j2)
-                    {
-                        rectGrid[nrow(rectGrid) + 1, ] = c(i, j, data[i, j])
+                for (j2 in j1:(j1 + 2)){
+                    if(j2 > 144){
+                        j2 = 144
                     }
-                }
-                p <- nrow(rectGrid)
-                MDsq <- calcMDsqFromMVG(rectGrid, meanBase, origYData, day,
-                                      data)
-                v = ifelse(p == 0, 0, pchisq(MDsq, p))
-                # color the grid boxes with value = +-MD
-                for(g in 1:nrow(rectGrid))
-                {
-                    val = max(abs(resToday[rectGrid[g, 1], rectGrid[g, 2]]),
-                              v)
-                    resToday[rectGrid[g, 1], rectGrid[g, 2]] = dir * val
+                    rectGrid <- data.frame(lat = numeric(), 
+                                           long = numeric(),
+                                           val = double())
+                    for(i in i1:i2)
+                    {
+                        for(j in j1:j2)
+                        {
+                            rectGrid[nrow(rectGrid) + 1, ] = c(i, j, data[i, j])
+                        }
+                    }
+                    p <- nrow(rectGrid) # number of grid cells in window
+                    MDsq <- calcMDsqFromMVG(rectGrid, meanBase, origYData, day,
+                                            data)
+                    v = ifelse(p == 0, 0, pchisq(MDsq, p))
+                    # color the grid boxes with value = +-MD
+                    for(g in 1:nrow(rectGrid))
+                    {
+                        val = max(abs(resToday[rectGrid[g, 1], rectGrid[g, 2]]),
+                                  v)
+                        resToday[rectGrid[g, 1], rectGrid[g, 2]] = dir * val
+                    }
                 }
             }
         }
     }
+    
+    # do mapping transformations
+    resToday <- t(resToday)
+    resToday <- resToday[, ncol(resToday):1]
+    
+    # map the result
+    # mapGriddedData(resToday, numCats = 21, catMethod = "diverging",
+    #                colourPalette = "palette", borderCol = "black")
+    
+    # cut at 5% at both tails
+    cutoff <- sort(abs(resToday), decreasing = T)[(0.05 * length(resToday))]
+    cutResToday <- resToday
+    cutResToday[cutResToday < cutoff & cutResToday > -cutoff] = 0
+    
+    # map the result
+    # mapGriddedData(cutResToday, numCats = 21, catMethod = "diverging",
+    #                colourPalette = "palette", borderCol = "black")
+    
+    # linearly stretch the values to [-1, 1] scale
+    negRange <- range(cutResToday[cutResToday < 0])
+    posRange <- range(cutResToday[cutResToday > 0])
+    cutResTodayPrime <- ifelse(cutResToday < 0,
+                               (cutResToday - negRange[1]) / 
+                                   (negRange[2] - negRange[1]) *
+                                   (-0.001 - (-1)) + (-1),
+                               cutResToday)
+    cutResTodayPrime <- ifelse(cutResToday > 0,
+                               (cutResToday - posRange[1]) / 
+                                   (posRange[2] - posRange[1]) *
+                                   (1 - (0.001)) + (0.001),
+                               cutResTodayPrime)
+    
+    # map the result
+    mapGriddedData(cutResTodayPrime, numCats = 21, catMethod = "diverging",
+                   colourPalette = "palette", borderCol = "black")
 }
 
-# do mapping transformations
-resToday <- t(resToday)
-resToday <- resToday[, ncol(resToday):1]
-
-# map the result
-# mapGriddedData(resToday, numCats = 21, catMethod = "diverging",
-#                colourPalette = "palette", borderCol = "black")
-
-# cut at 5% at both tails
-cutoff <- sort(abs(resToday), decreasing = T)[(0.05 * length(resToday))]
-cutResToday <- resToday
-cutResToday[cutResToday < cutoff & cutResToday > -cutoff] = 0
-
-# map the result
-mapGriddedData(cutResToday, numCats = 21, catMethod = "diverging",
-               colourPalette = "palette", borderCol = "black")
-
-# linearly stretch the values to [-1, 1] scale
-negRange <- range(cutResToday[cutResToday < 0])
-posRange <- range(cutResToday[cutResToday > 0])
-cutResTodayPrime <- ifelse(cutResToday < 0,
-                      (cutResToday - negRange[1]) / 
-                          (negRange[2] - negRange[1]) *
-                          (-0.001 - (-1)) + (-1),
-                      cutResToday)
-cutResTodayPrime <- ifelse(cutResToday > 0,
-                           (cutResToday - posRange[1]) / 
-                               (posRange[2] - posRange[1]) *
-                               (1 - (0.001)) + (0.001),
-                           cutResTodayPrime)
-# map the result
-mapGriddedData(cutResTodayPrime, numCats = 21, catMethod = "diverging",
-               colourPalette = "palette", borderCol = "black")
-
-# write csv
-#write.table(resToday, filename, row.names = F, col.names = F, sep = ",")
+#dev.off()
